e567cf7b6068788ab407c25f901f93d7ff8e2be4
Luca DEttorre <ldettorre21@gmail.com>
2018-06-05 08:32:12 +0000

    tweet web

---
+++ tweetweb.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask, render_template,redirect,request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def get_index():
+    return render_template("index.html")
+    
+    
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+
+
+
