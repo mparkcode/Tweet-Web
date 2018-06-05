08702a43de3f57ecfa389d7ffeb31a3e636fde33
Michael Park <mparkcode@gmail.com>
2018-06-05 14:11:13 +0000

    Wired up search form

--- search_tweets.py
+++ search_tweets.py
@@ -5,12 +5,8 @@ api = get_api()
 
 # enter a topic and return the info for number of tweets.
 def search(query,count): 
-    return [status for status in tweepy. Cursor(api.search, q = query).items(count)] #tweepys cursor is a prebuilt search feature made to use q = query for searching.
-
-tweets = (search("#kanye",10))     # topic, amount of tweets.
+    return [{'id': status.id, 'text': status.text} for status in tweepy. Cursor(api.search, q = query).items(count)] #tweepys cursor is a prebuilt search feature made to use q = query for searching.
 
 
-for tweet in tweets:        # for each tweet print out the text.
-    print(tweet.text)
 
 
--- tweetweb.py
+++ tweetweb.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, render_template,redirect,request
+from search_tweets import search
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ def get_index():
     
 @app.route("/results")
 def results():
-    return render_template("results.html")
+    q = request.args.get("q")
+    tweets = search(q,10)
+    return render_template("results.html", tweets=tweets)
     
     
 if __name__ == '__main__':
